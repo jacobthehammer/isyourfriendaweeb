77a1797b51a1a9778e5e2e7fbce43e322f9bb4d4
Jacob Hemmerich <jake@hemmerich.co>
2018-10-10 16:44:31 -0500

    Update styles.css
    Update LICENSE_HEAD

--- public/styles.css
+++ public/styles.css
@@ -1,23 +1,23 @@
 /*
- * @source: https://github.com/jacobthehammer/isyourfriendaweeb/tree/scripts
- * @license: https://www.gnu.org/licenses/gpl.txt
- * Copyright (c) 2018 Jacob A. Hemmerich
- * https://hemmerich.co
+ *@source: https://github.com/jacobthehammer/isyourfriendaweeb/tree/scripts
+ *@license: https://www.gnu.org/licenses/gpl.txt
+ *Copyright (c) 2018 Jacob A. Hemmerich
+ *https://www.hemmerich.co/
  *
- * This file is part of isyourfriendaweeb.
+ *This file is part of isyourfriendaweeb.
  *
- * isyourfriendaweeb is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *isyourfriendaweeb is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
  *
- * isyourfriendaweeb is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with isyourfriendaweeb.  If not, see <https://www.gnu.org/licenses/>.
+ *isyourfriendaweeb is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with isyourfriendaweeb.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 
